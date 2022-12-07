a8f506796c91efee0b3aa007c14eae1a29eb762b
Hy Murveit <hy-2@murveit.com>
2022-12-06 21:32:32 -0800

    Add Stats class--empty

---
+++ lpecprep/stats.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "stats.h"
+
+Stats::Stats(const PECData &samples)
+{
+}
+
+Stats::~Stats()
+{
+}
---
+++ lpecprep/stats.h
@@ -0,0 +1,15 @@
+#ifndef STATS_H
+#define STATS_H
+
+#include "structs.h"
+
+class Stats
+{
+    public:
+  Stats(const PECData &samples);
+        ~Stats();
+
+    private:
+};
+
+#endif // STATS
